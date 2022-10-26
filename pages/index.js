d60737a578b68acb8568ea1f7930e6e28764a70c
TheyCallMeCheng <cheng-corp@protonmail.com>
2022-10-26 18:28:44 +0200

    first prototype

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,44 @@
-import Head from 'next/head'
-import Image from 'next/image'
+
 
 export default function Home() {
   return (
     <div>
         <div className='main w-screen h-screen bg-black flex items-center justify-center overflow-hidden'>
             <div className='text-red-700  relative'>
-                <h1 className="text-4xl md:text-8xl">
-                    THE LIFE OF 
-                    <span className='pl-12 md:pl-40'>CHENG</span>
-                </h1>
-                <h1 className="text-4xl md:text-8xl">
-                    THE LIFE OF 
-                    <span className='pl-12'>CHENG</span>
-                </h1>
-                <h1 className="text-4xl md:text-8xl">
-                    THE LIFE OF 
-                    <span className='pl-12'>CHENG</span>
-                </h1>
-                <h1 className="text-4xl md:text-8xl">
-                    THE LIFE OF 
-                    <span className='pl-12'>CHENG</span>
-                </h1>
-
-                <img src='./ETH.jpg' className='absolute w-60 md:w-96 left-20'/>
-                
-                <h1 className="text-4xl md:text-8xl">
-                    THE LIFE OF 
-                    <span className='pl-12'>CHENG</span>
-                </h1>
-                <h1 className="text-4xl md:text-8xl">
-                    THE LIFE OF 
-                    <span className='pl-12'>CHENG</span>
-                </h1>
-                
-                <h1 className="text-4xl md:text-8xl pt-4">
-                    THE LIFE OF 
-                    <span className='pl-12 md:pl-40 '>CHENG</span>
-                </h1>
+                <div className=' '>
+                    <h1 className="text-4xl md:text-8xl">
+                        THE LIFE OF 
+                        <span className='pl-12 md:pl-40'>CHENG</span>
+                    </h1>
+                    <h1 className="text-4xl md:text-8xl">
+                        THE LIFE OF 
+                        <span className='pl-12'>CHENG</span>
+                    </h1>
+                    <h1 className="text-4xl md:text-8xl">
+                        THE LIFE OF 
+                        <span className='pl-12'>CHENG</span>
+                    </h1>
+                    <h1 className="text-4xl md:text-8xl">
+                        THE LIFE OF 
+                        <span className='pl-12'>CHENG</span>
+                    </h1>
+                    <div className='pic1'>
+                        <img src='./ETH.jpg' className='absolute w-60 md:w-96 md:left-20 left-10'/>
+                    </div>
+                    <h1 className="text-4xl md:text-8xl">
+                        THE LIFE OF 
+                        <span className='pl-12'>CHENG</span>
+                    </h1>
+                    <h1 className="text-4xl md:text-8xl">
+                        THE LIFE OF 
+                        <span className='pl-12'>CHENG</span>
+                    </h1>
+                    
+                    <h1 className="text-4xl md:text-8xl md:pt-4 pt-16">
+                        THE LIFE OF 
+                        <span className='pl-12 md:pl-40 '>CHENG</span>
+                    </h1>
+                </div>
                 <div className='flex justify-between'>
                     <div>
                         <h3 className='text-2xl md:text-4xl'>
@@ -79,7 +80,7 @@ export default function Home() {
                             WHICH / ONE
                         </h3>
     
-                        <img src='./DEV.png' className='absolute h-60 right-32'/>
+                        <img src='./DEV.png' className='absolute h-32 md:h-60 md:right-32 right-16'/>
 
                         <h3 className='text-2xl md:text-4xl'>
                             WHICH / ONE
