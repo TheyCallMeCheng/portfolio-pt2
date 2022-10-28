9ac0b903b74da0f209ac35f65ebcbdf08feec526
TheyCallMeCheng <cheng-corp@protonmail.com>
2022-10-28 15:38:45 +0200

    fix: blur, centered text

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
-
-
 export default function Home() {
   return (
     <div>
         <div className='main w-screen h-screen bg-black flex items-center justify-center overflow-hidden'>
-            <div className='text-red-700  relative'>
-                <div className=' '>
+            <div className='text-red-700  relative parent'>
+                <div className='test'>
                     <h1 className="text-4xl md:text-8xl">
                         THE LIFE OF 
                         <span className='pl-12 md:pl-40'>CHENG</span>
@@ -22,8 +20,16 @@ export default function Home() {
                         THE LIFE OF 
                         <span className='pl-12'>CHENG</span>
                     </h1>
-                    <div className='pic1'>
-                        <img src='./ETH.jpg' className='absolute w-60 md:w-96 md:left-20 left-10'/>
+                    <div className='pic1 absolute w-60 md:w-96 h-60 md:left-20 left-10'>
+                        <img src='./ETH.jpg' className='absolute  h-60 cursor-pointer main-img rounded-lg'/>
+                        <a className="text-2xl text-black font-bold link hidden absolute text-center" href="#">
+                            WEB3 PROJECTS
+                        </a>
+
+                        <img src='./ETH_LOGO.png' className='absolute w-20 md:w-32 md:left-52 left-10  pic1-hide cursor-pointer hidden'/>
+                        <img src='./HARDHAT_LOGO.png' className='absolute w-20 md:w-32 md:left-52 left-10  pic2-hide cursor-pointer hidden'/>
+                        <img src='./SOLIDITY_LOGO.jpg' className='absolute w-20 md:w-32 md:left-52 left-10  pic3-hide cursor-pointer hidden rounded-lg'/>
+                        <img src='./NFT_LOGO.png' className='absolute w-20 md:w-32 md:left-52 left-10  pic4-hide cursor-pointer hidden rounded-lg'/>
                     </div>
                     <h1 className="text-4xl md:text-8xl">
                         THE LIFE OF 
@@ -34,7 +40,7 @@ export default function Home() {
                         <span className='pl-12'>CHENG</span>
                     </h1>
                     
-                    <h1 className="text-4xl md:text-8xl md:pt-4 pt-16">
+                    <h1 className="text-4xl md:text-8xl md:pt-20 pt-16">
                         THE LIFE OF 
                         <span className='pl-12 md:pl-40 '>CHENG</span>
                     </h1>
@@ -80,7 +86,7 @@ export default function Home() {
                             WHICH / ONE
                         </h3>
     
-                        <img src='./DEV.png' className='absolute h-32 md:h-60 md:right-32 right-16'/>
+                        <img src='./DEV.png' className='absolute h-32 md:h-60 md:right-32 right-16 rounded-lg'/>
 
                         <h3 className='text-2xl md:text-4xl'>
                             WHICH / ONE
